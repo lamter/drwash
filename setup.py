5354885b767718e9d15727369036ba60326bc0b5
lamter <lamter.fu@gmail.com>
2017-06-27 20:45:29 +0800

    - add : 添加 Pypi 安装包

---
+++ setup.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+__version__ = "0.0.1"
+
+setup(
+    name='drwash',
+    version=__version__,
+    keywords='drwash',
+    description=u'清洗vnpy记录的数据',
+    long_description=read("README.md"),
+
+    url='https://github.com/lamter/drwash',
+    author='lamter',
+    author_email='lamter.fu@gmail.com',
+
+    packages=find_packages(),
+    package_data={
+
+    },
+    install_requires=read("requirements.txt").splitlines(),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License'],
+)
